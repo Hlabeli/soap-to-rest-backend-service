2d1715674714f831c3178d4ea60e5afef130f307
Hlabeli <hlabeli.zee@gmail.com>
2024-11-11 06:12:27 +0200

    First commit

---
+++ src/main/java/com/example/SoapToRestBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapToRestBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapToRestBackendServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/route/EmployeeRoute.java
@@ -0,0 +1,10 @@
+package com.example.route;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class EmployeeRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
